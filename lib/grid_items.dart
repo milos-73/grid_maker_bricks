03b2da243d230cb9fbe4e2d440fe02aa47110863
milos-73 <admin@viatatry.eu>
2023-09-21 19:53:52 +0200

    Implementované vytváranie Lists a ich uloženie na SharePoint

--- lib/grid_items.dart
+++ lib/grid_items.dart
@@ -1,8 +1,11 @@
 
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:grid_maker_bricks/hex_color.dart';
 import 'package:grid_maker_bricks/provider_color.dart';
+import 'package:grid_maker_bricks/walls.dart';
 import 'package:provider/provider.dart';
 
 import 'color_numbers.dart';
@@ -10,8 +13,7 @@ import 'color_numbers.dart';
 class ItemTile extends StatefulWidget {
   final int itemNo;
 
-  const ItemTile(
-      this.itemNo, {super.key});
+  const ItemTile(this.itemNo, {super.key});
 
   @override
   State<ItemTile> createState() => _ItemTileState();
@@ -21,6 +23,7 @@ class _ItemTileState extends State<ItemTile> {
 
   ColorNumbers colorNumbers = ColorNumbers();
   bool? _color;
+  BrickWalls brickWalls = BrickWalls();
 
   @override
   void initState() {
@@ -30,12 +33,14 @@ class _ItemTileState extends State<ItemTile> {
 
   @override
   Widget build(BuildContext context) {
-    var activeColorNumber = Provider.of<BrickColorNumber>(context, listen: false).index;
-    return ListTile(
+
+      return ListTile(
       shape: const BeveledRectangleBorder(side: BorderSide(color: Colors.black,width: 0.2)),
-      tileColor: _color == true && activeColorNumber == 0 ? HexColor('#d6d6d4') : HexColor(colorNumbers.getColor(activeColorNumber!)),
-      onTap: () {setState(() {_color = !_color!;});
-        print('_COLOR: ${_color}');},
-   );
+      tileColor: _color == true && Provider.of<BrickColorNumber>(context, listen: false).index == 0 ? HexColor('#d6d6d4') : HexColor(colorNumbers.getColor(Provider.of<BrickColorNumber>(context, listen: false).index ?? 0)),
+      onTap: () {
+      setState(() {_color = !_color!;});
+      brickWalls.addBrickTypeToList(context, widget.itemNo);
+        },
+        );
   }
 }
---
+++ lib/walls.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:grid_maker_bricks/provider_color.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+List brickList =
+  [
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 25, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+    [00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00],
+ ];
+
+ int? wallNumber;
+
+
+class BrickWalls {
+
+
+  Future<void> addBrickTypeToList(context, int index) async {
+
+    int? row;
+    int? column;
+    int? color;
+    int? wallNumber;
+
+    column = (index % 11);
+    row = (index / 11).round();
+
+    color = (Provider.of<BrickColorNumber>(context, listen: false).index);
+    color != null ? color = color + 10 : color = 0;
+
+    brickList[row][column] = color;
+
+    var prefs = await SharedPreferences.getInstance();
+    wallNumber = prefs.getInt('wallNumber') ?? 0;
+    prefs.setString('wall$wallNumber', jsonEncode(brickList));
+    prefs.setInt('wallNumber', wallNumber + 1);
+    jsonDecode(prefs.getString('wall$wallNumber') ?? '');
+
+  }
+
+}
